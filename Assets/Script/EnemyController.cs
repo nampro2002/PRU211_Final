121b982c5d681d0159efc026f3b602fc69b4f0e9
Phong Phạm <phongvupham1110@gmail.com>
2023-10-30 17:50:26 +0700

    Test2

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
